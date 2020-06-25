34970f03dcd541ffee06e47a60e3470523f724c5
Mees van Dijk <mees@mees.io>
2020-06-25 15:12:31 +0200

    Clean up multiple exports from the same file

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@
 // export default createGame
 // export const validAction = validAction
 
-export { default, restoreGame } from "./state/index"
+export { default, restoreGame, RootState } from "./state/index"
 export { default as validAction } from "./state/validActions"
 export { PlayCard, Skip, Action } from "./state/actions"
-export { RootState } from "./state/index"
