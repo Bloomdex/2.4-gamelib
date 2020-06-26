a28328182873577791477ca7a745ad40b224f608
Mees van Dijk <mees@mees.io>
2020-06-26 12:32:18 +0200

    Add test

---
+++ test/Game.test.ts
@@ -0,0 +1,20 @@
+import test from "ava"
+import createGame, { validActions } from "../src/index"
+import pesten from "../rulesets/pesten"
+
+test("my life", (t) => {
+	const game = createGame({
+		players: 3,
+		gameRules: pesten,
+		seed: "b",
+	})
+	game.dispatch(validActions(game.getState())[0])
+	// if it didn't throw I'm happy
+	t.pass()
+})
+
+const logActions = (game: ReturnType<typeof createGame>) => {
+	validActions(game.getState()).forEach((action) => {
+		console.log(JSON.stringify(action))
+	})
+}
