365f1c35c38b1b4b9220a2e8baeaef548a8e01bb
Mees van Dijk <mees@mees.io>
2020-06-26 12:30:14 +0200

    Fix arrays getting messed up
    Man this was an  annoying bug. the resolveOptions function was changing
    arrays into objects with "0", "1", etc as keys. Which made them not
    iterable. We thought it was firebase's fault, fixed it there. The issue still
    persisted. I think it took us about an hour to find this function as the
    cause.

--- src/Card.ts
+++ src/Card.ts
@@ -41,8 +41,11 @@ JSON.parse(`
 type OptionsMap = Record<string, any>
 
 export function resolveOptions(obj: any, options: OptionsMap) {
+	if (Array.isArray(obj)) {
+		return resolveOptionsArr(obj, options)
+	}
 	const newObj = { ...obj }
-	for (const [key, value] of Object.entries(obj)) {
+	for (let [key, value] of Object.entries(obj)) {
 		if (typeof value === "string" && value in options) {
 			newObj[key] = options[value]
 		} else if (typeof value === "object") {
@@ -50,4 +53,17 @@ export function resolveOptions(obj: any, options: OptionsMap) {
 		}
 	}
 	return newObj
+}
+
+function resolveOptionsArr(arr: Array<any>, options: OptionsMap) {
+	const newArr = [...arr]
+	for (let key = 0; key < arr.length; key++) {
+		const value = arr[key]
+		if (typeof value === "string" && value in options) {
+			newArr[key] = options[value]
+		} else if (typeof value === "object") {
+			newArr[key] = resolveOptions(value, options)
+		}
+	}
+	return newArr
 }
